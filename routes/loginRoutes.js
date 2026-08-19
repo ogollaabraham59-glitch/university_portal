fb0a69057fd8c995366421bb8e1033e7052cce8e
Abraham <ogollaabraham59@gmail.com>
2026-08-19 14:01:23 +0300

    chanhe

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router()
 const loginControllers = require('../controllers/loginControllers')
 
 //login routes
-router.post('/register', loginControllers.registerAdmin)
-router.post('/', loginControllers.logIn)
-
+router.post('/register', loginControllers.registerUser);
+router.post('/login', loginControllers.logIn);
 module.exports = router
